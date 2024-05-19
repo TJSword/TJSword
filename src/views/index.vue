0c7d33db7c6c0630f2079421e0d93917531f96b0
TJSword <yangxianbiao@norinaviation.com>
2024-05-19 15:43:25 +0800

    测试：首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,12 +1,36 @@
 
 <template>
   <div class="wrap">
-    <div ref="map" id="map">123</div>
+    <div ref="mapContainer" id="map"></div>
 
   </div>
 </template>
 
 <script lang="ts" setup>
-  import zhCn from 'element-plus/es/locale/lang/zh-cn'
-  let locale = zhCn
+  import mapboxgl from 'mapbox-gl'
+  const mapContainer = ref(null)
+  const initMap = () => {
+      mapboxgl.accessToken =
+          'pk.eyJ1Ijoib25pc205IiwiYSI6ImNsaGV6empibDAzd3gzdHBmeGd2bDAwYWkifQ.gjhG1z8Uo8bZETQuRn3Stg'
+      const map = new mapboxgl.Map({
+          container: mapContainer.value,
+          center: [0, 0], // starting position [lng, lat]
+          zoom: 9 // starting zoom
+      })
+
+      map.addControl(new mapboxgl.NavigationControl())
+  }
+  onMounted(() => {
+      initMap()
+  })
 </script>
+<style lang="scss" scoped>
+  .wrap {
+      height: 100vh;
+      width: 100vw;
+      #map {
+          height: 100vh;
+          width: 100vw;
+      }
+  }
+</style>
