126947df2512c72ef4d97da605bd8d5ca74e441a
yangxianbiao <yangxianbiao@norinaviation.com>
2024-05-19 15:16:16 +0800

    首次提交

---
+++ src/views/error/index.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <div class="wrap">
+    <div ref="map" id="map"></div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import zhCn from 'element-plus/es/locale/lang/zh-cn'
+  let locale = zhCn
+</script>
